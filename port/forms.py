cdcf2b807749a9ff6a1f067bdec20ec14147d84c
derick-droid <derrickokinda9@gmail.com>
2022-11-11 17:28:05 +0300

    style the django forms

--- port/forms.py
+++ port/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class Contact(forms.Form):
-    name = forms.CharField(max_length=200)
-    email  = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)    
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'Email', 'style': 'width: 300px;', 'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={"placeholder": "Enter your message", "style":"width: 500px;", "class":"form-control"}))    
