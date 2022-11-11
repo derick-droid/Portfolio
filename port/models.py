34d4f096e4d2bf9094f7f1fba4b36b5ca99f6d8e
derick-droid <derrickokinda9@gmail.com>
2022-11-11 23:20:51 +0300

    portfolio

--- port/models.py
+++ port/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Projects(models.Model):
+class Project(models.Model):
     project_name = models.CharField(max_length=255)
     created_date = models.DateTimeField(auto_now_add=True)
     project_link = models.URLField(max_length=255)
