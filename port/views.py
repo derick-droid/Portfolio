9c71d888b7a7f574f61364e8cfcadb0f0b0e8da4
derick-droid <derrickokinda9@gmail.com>
2022-11-12 20:49:50 +0300

    send mail

--- port/views.py
+++ port/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, HttpResponse
 from .forms import Contact
+from django.core.mail import send_mail
 
 
 def home(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        message = request.POST["message"]
+        
+        send_mail(
+            "message from" + name,
+            message,
+            email,
+            ["derrickokinda9@gmail.com", "developer.derrickokinda9@gmail.com"]
+            
+        )
     form = Contact
     context = {
         "form":form
     }
     return render(request, "port/home.html", context)
 
-def contact(request):
-   
-    form = Contact
-    context ={
-        "form":form
-    }
-    
-    return render (request, "port/contact.html", context)
